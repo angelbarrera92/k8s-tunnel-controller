ac34fc4f3596769123af93c4b5c5d5d1c7fa5855
Barrera, Angel <Angel.Barrera@adidas.com>
2021-11-30 08:58:28 +0100

    Add labels to the pod to identify it as a tunnel pod.

--- src/controller/tunnel/tunnel.py
+++ src/controller/tunnel/tunnel.py
@@ -11,12 +11,21 @@ logger = getLogger(__name__)
 def tunnel(api, svc: Service, port: int, subdomain: str):
     logger.info(
         f"ensuring tunnel for service {svc.namespace}/{svc.name} in the port {port} on {subdomain} subdomain")
+    name = f"{svc.name}-{port}-tunnel"
     podSpec = {
         "apiVersion": "v1",
         "kind": "Pod",
         "metadata": {
-            "name": subdomain,
+            "name": name,
             "namespace": svc.namespace,
+            "labels": {
+                "app.kubernetes.io/name": "tunnel",
+                "app.kubernetes.io/instance": name,
+                "app.kubernetes.io/version": "v1",
+                "app.kubernetes.io/component": "tunnel",
+                "app.kubernetes.io/part-of": f"svc/{svc.name}",
+                "app.kubernetes.io/managed-by": "k8s-tunnel-controller"
+            }
         },
         "spec": {
             "automountServiceAccountToken": False,
@@ -49,4 +58,3 @@ def tunnel(api, svc: Service, port: int, subdomain: str):
 # TODO. Need to set the resources as low as possible.
 # TODO. Evaluate setting up my own host and make it configurable
 # TODO. This is an optimistic approach. Ideally, the new pod should notify somehow the controller that is ready to accept requests.
-# TODO. Add labels to the pod to identify it as a tunnel pod.
