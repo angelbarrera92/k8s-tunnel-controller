f8d50a0b7b930c6ab013da311e269bf6d498a8f6
angelbarrera92 <angelbarrerasanchez@protonmail.com>
2021-12-12 19:14:06 +0100

    FIX: pods is not iterable

--- src/kubernetes/pods.py
+++ src/kubernetes/pods.py
@@ -4,8 +4,9 @@ from src.kubernetes.connection import kubernetes_api
 
 @kubernetes_api
 def get(api, namespace: str, labels: dict = {}) -> Pod:
-    pods = Pod.objects(api, namespace=namespace).filter(
-        namespace=namespace, selector=labels)
+    pods = list()
+    for p in Pod.objects(api, namespace=namespace).filter(namespace=namespace, selector=labels):
+        pods.append(p)
     if len(pods) == 1:
         return pods[0]
     return None
